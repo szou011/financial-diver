4545d08fdf1f2bc3bde81f3c12fc30ffe55d5b8f
Stephen Zou <zou.stephen@gmail.com>
2022-08-26 17:56:01 +1200

    test data run

--- database.py
+++ database.py
@@ -1,12 +1,5 @@
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
 from orm import *
 
 engine = create_engine("sqlite+pysqlite:///./database/exo.db", echo=False, future=True)
-
-statement = select(Debtor.ACCNO, Currency.CURRCODE).join(Debtor.debtor_currency).where(Debtor.ACCNO == 9901)
-
-with Session(engine, future=True) as session:
-    result = session.execute(statement).all()
-    print(result)
--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-from database import acc_group, group_count
-
-from bokeh.layouts import column
+from bokeh.layouts import column, row
 from bokeh.plotting import figure, curdoc
+from bokeh.models import Select
+
+from models import get_retailer, get_retailgroup
+
+groupname, groupcount = get_retailer("Farmers")
+
 
 # create a plot and style its properties
-p = figure(width=400, height=400)
+p = figure(x_range=groupname, width=1600, height=400, toolbar_location=None)
 
 # add a text renderer to the plot (no data yet)
-p.vbar(x=acc_group, top=group_count, width=0.5, bottom=0)
+r = p.vbar(x=groupname, top=groupcount, width=0.9, bottom=0)
+print(r.data_source.data)
+p.xaxis.major_label_orientation = 1
 
+ds = r.data_source
 
+def update_retailgroup(attr, old, new):
+    global groupname, groupcount
+    groupname, groupcount = get_retailer(new)
+    new_data = {'x':groupname, 'top':groupcount}
+    print(new_data)
+    p.x_range.factors = groupname
+    ds.data = new_data  
+
+select = Select(title='Select Retail Group:', value='Farmers', options=get_retailgroup())
+select.on_change('value', update_retailgroup)
 # put the button and plot in a layout and add to the document
-curdoc().add_root(column(p))
+curdoc().add_root(row(select, p))
