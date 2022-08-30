ee4fad10c80566b2a56fd633630dc5b50d2a81ad
Stephen Zou <zou.stephen@gmail.com>
2022-08-30 15:43:12 +1200

    intergrate with Flask

---
+++ serve.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+from bokeh.client import pull_session
+from bokeh.embed import server_session
+
+app_url = "http://localhost:5100/bokeh_app"
+
+app = Flask(__name__)
+
+@app.route('/')
+def bkapp_page():
+
+    # pull a new session from running Bokeh server
+    with pull_session(url=app_url) as session:
+
+        # generate a script to load the customized session
+        script = server_session(session_id=session.id, url=app_url)
+
+        # use the script in the rendered page
+        return render_template("embed.html", script=script, template="Flask")
+
+if __name__ == '__main__':
+    app.run(port=8080)
