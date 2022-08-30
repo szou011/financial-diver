85d405267f7703c50500d65d5465858e721de069
Stephen Zou <zou.stephen@gmail.com>
2022-08-30 12:35:41 +1200

    include __init__.py to construct a package

--- database.py
+++ database.py
@@ -2,4 +2,4 @@ from sqlalchemy import create_engine
 
 from orm import *
 
-engine = create_engine("sqlite+pysqlite:///./database/exo.db", echo=False, future=True)
+engine = create_engine("sqlite+pysqlite:////Users/StephenZ/WPy64_3950/scripts/source/financial-diver/database/exo.db", echo=False, future=True)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from bokeh.layouts import column, row
-from bokeh.plotting import figure, curdoc
+from bokeh.plotting import figure, row
+from bokeh.io import curdoc
 from bokeh.models import Select
 
 from models import get_retailer, get_retailgroup
@@ -8,11 +8,10 @@ groupname, groupcount = get_retailer("Farmers")
 
 
 # create a plot and style its properties
-p = figure(x_range=groupname, width=1600, height=400, toolbar_location=None)
+p = figure(x_range=groupname, width=640, height=400, toolbar_location=None, name='bokeh_jinja_figure')
 
 # add a text renderer to the plot (no data yet)
 r = p.vbar(x=groupname, top=groupcount, width=0.9, bottom=0)
-print(r.data_source.data)
 p.xaxis.major_label_orientation = 1
 
 ds = r.data_source
@@ -21,11 +20,10 @@ def update_retailgroup(attr, old, new):
     global groupname, groupcount
     groupname, groupcount = get_retailer(new)
     new_data = {'x':groupname, 'top':groupcount}
-    print(new_data)
     p.x_range.factors = groupname
     ds.data = new_data  
 
-select = Select(title='Select Retail Group:', value='Farmers', options=get_retailgroup())
+select = Select(title='Select Retail Group:', value='Farmers', options=get_retailgroup(), name='bokeh_jinja_widget')
 select.on_change('value', update_retailgroup)
 # put the button and plot in a layout and add to the document
 curdoc().add_root(row(select, p))
